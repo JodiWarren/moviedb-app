962ad6abd172d64b0a3cb42ab11a82abc7604562
jodi Warren <jodi@jodiwarren.com>
2018-04-22 17:57:56 +0100

    Basic movie search working

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,55 @@ import { TmdbApi } from "@jodi/tmdb-typescript-api";
 import * as React from 'react';
 
 import './App.css';
-import logo from './logo.svg';
 
 import {key} from "./config/config";
 
 class App extends React.Component {
 
+  public api: TmdbApi | null = null;
+
   public state = {
     movies: {}
   };
 
+  public input: HTMLInputElement | null = null;
+
   public componentDidMount() {
-    const api = new TmdbApi(key);
-    api.search.movies('america').subscribe(movies => {
-      this.setState({movies})
-    });
+    this.api = new TmdbApi(key);
+  }
+
+  public handleSubmit = (event: React.FormEvent<HTMLFormControlsCollection>) => {
+    event.preventDefault();
+    if (this.api === null || this.input === null) {
+      return;
+    }
+    this.api.search.movies(this.input.value).subscribe(movies => {
+      this.setState({movies});
+    })
   }
 
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Movie App</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and saaave to reload.
-          <pre>
+          <form id="searchMovies" className="searchMovies" onSubmit={this.handleSubmit}>
+            <label htmlFor="">Search Movies</label>
+            <input
+              type="search"
+              id="movieSearchField"
+              placeholder="Amelie"
+              ref={(input) => this.input = input}
+            />
+          </form>
+        </p>
+        <pre className="api-results">
             {
               JSON.stringify(this.state.movies, null, 2)
             }
-          </pre>
-        </p>
+        </pre>
       </div>
     );
   }
